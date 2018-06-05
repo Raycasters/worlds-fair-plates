6b32af800580e06511bd3130d9f53408bde6f28d
Sam <splavigne@gmail.com>
2018-06-05 18:01:44 -0400

    label plates

--- scrapers.py
+++ scrapers.py
@@ -3,7 +3,10 @@ import os
 import pytz
 import json
 import datetime
+import time
 import requests
+import numpy as np
+import tensorflow as tf
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.shopping import Connection as Shopping
 from ebaysdk.exception import ConnectionError
@@ -194,6 +197,99 @@ def download_images():
             listing.save()
 
 
+def load_graph(model_file):
+    graph = tf.Graph()
+    graph_def = tf.GraphDef()
+
+    with open(model_file, "rb") as f:
+        graph_def.ParseFromString(f.read())
+    with graph.as_default():
+        tf.import_graph_def(graph_def)
+
+    return graph
+
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299, input_mean=0, input_std=255):
+
+    input_name = "file_reader"
+    output_name = "normalized"
+    file_reader = tf.read_file(file_name, input_name)
+    if file_name.endswith(".png"):
+        image_reader = tf.image.decode_png(file_reader, channels=3, name='png_reader')
+    elif file_name.endswith(".gif"):
+        image_reader = tf.squeeze(tf.image.decode_gif(file_reader, name='gif_reader'))
+    elif file_name.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(file_reader, channels = 3, name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0);
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run(normalized)
+
+    return result
+
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
+
+
+def label_images():
+    model_file = "retrained_graph.pb"
+    label_file = "retrained_labels.txt"
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    input_layer = "input"
+    output_layer = "final_result"
+
+    labels = load_labels(label_file)
+    graph = load_graph(model_file)
+
+    listings = Listing.objects.exclude(image='default.jpg').exclude(image=None).exclude(confirmed=True).filter(plate=None)
+
+    with tf.Session(graph=graph) as sess:
+        for l in listings:
+            file_name = l.image
+            if not os.path.exists(file_name):
+                continue
+
+            t = read_tensor_from_image_file(file_name, input_height=input_height, input_width=input_width, input_mean=input_mean, input_std=input_std)
+
+            input_name = "import/" + input_layer
+            output_name = "import/" + output_layer
+            input_operation = graph.get_operation_by_name(input_name);
+            output_operation = graph.get_operation_by_name(output_name);
+
+            results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: t})
+
+            results = np.squeeze(results)
+            top_k = results.argsort()[-5:][::-1]
+            label=labels[top_k[0]]
+            confidence = results[top_k[0]]
+
+            print(file_name)
+            print("{} (score={:0.5f})".format(label, confidence))
+            print('--')
+
+            plate = Plate.objects.get(label=label)
+            print(plate.title)
+            l.plate = plate
+            l.confidence = confidence
+            l.save()
+            # print(top_k)
+            # for i in top_k:
+            #     print(template.format(labels[i], results[i]))
+
+
+
 if __name__ == '__main__':
 
     keywords = ["1964 world's fair plate new york"]
@@ -206,6 +302,8 @@ if __name__ == '__main__':
         parse_and_save_etsy(results)
 
     download_images()
+
+    label_images()
 
     # results = search_ebay(keywords[0], 'findItemsAdvanced')
     # with open('ebay3.json', 'w') as outfile:
