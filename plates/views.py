9678667b2c6348a8ed0a4e09ab9d389db6419c9d
Sam <splavigne@gmail.com>
2018-07-18 19:54:02 -0400

    check confidence

--- plates/views.py
+++ plates/views.py
@@ -19,7 +19,8 @@ def plate_list(request):
     if request.GET.get('listings'):
         for p in all_plates:
             listings = Listing.objects.all().filter(plate_id=p['id'])
-            listings = listings.filter(confirmed=True)
+            listings = listings.filter(confidence__gt=0.5)
+            listings = listings.filter(confirmed=True).order_by('-confidence')
             if request.GET.get('listing_limit'):
                 limit = int(request.GET.get('listing_limit'))
                 listings = listings[0:limit]
@@ -37,7 +38,8 @@ def plate_details(request, pk):
         'id': plate.id, 'title': plate.title, 'description': plate.description, 'image': plate.image
     }
     listings = Listing.objects.all().filter(plate_id=output['id'])
-    listings = listings.filter(confirmed=True)
+    listings = listings.filter(confidence__gt=0.5)
+    listings = listings.filter(confirmed=True).order_by('-confidence')
     if request.GET.get('listing_limit'):
         limit = int(request.GET.get('listing_limit'))
         listings = listings[0:limit]
@@ -57,12 +59,15 @@ def listings(request):
 
     if query.get('confirmed'):
         all_listings = all_listings.filter(confirmed=True)
+        all_listings = all_listings.filter(confidence__gt=0.5)
 
     if query.get('limit'):
         all_listings = all_listings[0:int(query.get('limit'))]
 
     if query.get('notplates'):
         all_listings = all_listings.filter(not_a_plate=True)
+
+    all_listings = all_listings.order_by('-confidence')
 
     all_listings = all_listings.values('id', 'title', 'plate_id', 'listing_source', 'listing_url', 'price', 'date_listed', 'image', 'lat', 'lng')
 
